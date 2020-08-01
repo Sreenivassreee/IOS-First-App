f3d2b9749b7ed4b210bf328b75ce9013a981a77e
Sreenivas K <ksreenivascse@gmail.com>
2020-08-01 14:56:08 +0530

    This is my First app and First commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Sreenivas K on 01/08/20.
+//  Copyright © 2020 Sreenivas K. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Sreenivas \n Welcome to IOS !")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
